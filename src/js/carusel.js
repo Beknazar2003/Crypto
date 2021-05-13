12c025cd287f3a24ba4d995caacb7a20c8669fba
Beknazar2003 <bektashev2003@mail.ru>
2021-05-14 01:44:47 +0600

    initial

--- src/js/carusel.js
+++ src/js/carusel.js
@@ -1,15 +1,32 @@
+const start = (screen) => {
+  console.log('screen ', screen)
+
+  $('.autoplay-carusel')
+    .not('.slick-initialized')
+    .slick({
+      slidesToShow: screen > 900 ? 3 : 2,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      appendArrows: $('.autoplay-carusel'),
+      prevArrow:
+        '<i class="fa fa-angle-left btn-carusel" aria-hidden="true"></i>',
+      nextArrow:
+        '<i class="fa fa-angle-right btn-carusel" aria-hidden="true"></i>',
+    })
+  $('.card-verlical-list')
+    .not('.slick-initialized')
+    .slick({
+      slidesToShow: screen > 900 ? 4 : 2,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    })
+}
+
 $(document).ready(() => {
-  $('.autoplay-carusel').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    appendArrows: $('.autoplay-carusel'),
-    prevArrow:
-      '<i class="fa fa-angle-left btn-carusel" aria-hidden="true"></i>',
-    nextArrow:
-      '<i class="fa fa-angle-right btn-carusel" aria-hidden="true"></i>',
-    centerPadding: 0,
-    customPaging: 0,
+  start(window.screen.width)
+  $(window).resize(() => {
+    start(window.screen.width)
   })
 })
