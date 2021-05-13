5ec9b439b2e10ede523250d4e0b68db9fb038e07
Beknazar2003 <bektashev2003@mail.ru>
2021-05-14 00:37:32 +0600

    initial

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,4 +1,7 @@
 $(document).ready(() => {
-  $('[data-trigger]').click(() => $('#navbar_main').addClass('show'))
+  $('[data-trigger]').click(() => {
+    $('#navbar_main').addClass('show navbar-light')
+    $('#navbar_main').removeClass('navbar-dark')
+  })
   $('.btn-closer').click(() => $('.mobile-offcanvas.show').removeClass('show'))
 })
